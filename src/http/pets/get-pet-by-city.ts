e4bf618b1fcb3ac9112eb63e07770f5565c21423
unknown <vi.palca@gmail.com>
2025-04-23 11:50:40 -0300

    Controller get pet by city

---
+++ src/http/pets/get-pet-by-city.ts
@@ -0,0 +1,29 @@
+import { PrismaPetRepository } from '@/repositories/prisma/prisma-pets-repository'
+import { ResourceNotFoundError } from '@/services/erros/resource-not-found'
+import { GetPetByCityService } from '@/services/pets/get-pet-by-city'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function getPetByCity(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const paramSchema = z.object({
+    city: z.string(),
+  })
+
+  const { city } = paramSchema.parse(request.params)
+
+  try {
+    const prismaPetsRepository = new PrismaPetRepository()
+    const getPetByCityService = new GetPetByCityService(prismaPetsRepository)
+
+    const { pets } = await getPetByCityService.execute({ city })
+
+    reply.status(200).send({
+      pets,
+    })
+  } catch (error) {
+    return reply.status(500).send({ message: `${error}` })
+  }
+}
